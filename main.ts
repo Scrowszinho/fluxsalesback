4197a2ee601be637db923ad3e702b40452ce3a8d
Gustavo <gustascrows@gmail.com>
2023-10-24 19:53:17 -0300

    feat: comecando integração

--- main.ts
+++ main.ts
@@ -6,11 +6,10 @@ import auth0 from './src/config/auth0';
 const app = express();
 const port = 3000;
 app.use(express.json());
-app.use(routes)
-app.use(auth(auth0))
-
+app.use(routes);
+app.use(auth(auth0));
 app.get('/', (req, res) => {
-  res.send({hello: 'world'});
+  res.send({ hello: 'world' });
 });
 
 app.listen(port, () => {
--- src/config/auth0.ts
+++ src/config/auth0.ts
@@ -3,7 +3,7 @@ const config = {
     auth0Logout: true,
     secret: 'a long, randomly-generated string stored in env',
     baseURL: 'http://localhost:3000',
-    clientID: 'nuznypU8W5ZwGs5vpbOQ2equZhrBrBGn',
+    clientID: 'bRZOOPTcSiEG5SOlbAuYw2nbDy2LnBeD',
     issuerBaseURL: 'https://dev-3yg4atan6fctzshg.us.auth0.com'
 }
 

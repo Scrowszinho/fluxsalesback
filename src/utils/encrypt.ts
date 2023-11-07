c5aaf011397ef9b6a7951fd210f220ef352b0ba0
Gustavo <gustascrows@gmail.com>
2023-11-07 20:50:43 -0300

    feat:  removendo bycrpt

--- src/utils/encrypt.ts
+++ src/utils/encrypt.ts
@@ -1,13 +1,10 @@
-import bcrypt from 'bcrypt';
 
 export const hashPassword = async (password: string)  =>{
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-    return hash;
+   
+    return password;
 }
 
 export const checkPassword = async (password: string, storedHash: string) => {
-    const result = await bcrypt.compare(password, storedHash);
-    return result;
+    return password;
 }
   

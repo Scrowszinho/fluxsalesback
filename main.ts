32798ef1ebefe711e621773c5dd6ea505da79398
Gustavo <gustascrows@gmail.com>
2023-12-26 19:36:23 -0300

    fix: correcao

--- main.ts
+++ main.ts
@@ -2,17 +2,17 @@ import express from 'express';
 import routes from './src';
 import cors, { CorsOptions } from 'cors';
 const app = express();
-import session from 'express-session';
-import passport from './src/modules/auth/Passport-config';
+// import session from 'express-session';
+// import passport from './src/modules/auth/Passport-config';
 
 const corsOptions : CorsOptions = {
   origin: 'http://localhost:4200',
   optionsSuccessStatus: 200
 }
 
-app.use(session({ secret: 'secretpass', resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(session({ secret: 'secretpass', resave: true, saveUninitialized: true }));
+// app.use(passport.initialize());
+// app.use(passport.session());
 app.use(express.json());
 app.use(cors(corsOptions))
 app.use(routes);
